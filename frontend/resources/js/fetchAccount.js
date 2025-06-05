adbfa9601eebca822d26c3abcb9045eaf371d9cc
bad0dab <broadnicegamerr@gmail.com>
2025-06-05 15:18:46 +0200

    automatically creates json with account attributes from db

--- frontend/resources/js/fetchAccount.js
+++ frontend/resources/js/fetchAccount.js
@@ -1,33 +1,22 @@
-const {Client} = require('pg')
+const { Client } = require('pg');
 
-const con = new Client( {
-    host : 'localhost',
-    user : "postgres",
-    port : 8000,
-    password : '1234',
-    database : 'bankaccount'
-
-})
-con.connect().then(()=> console.log("Connected"))
-    con.query('SELECT * FROM account', (err, res) => {
-    if (err) {
-        console.error('Errore nella query:', err);
-    } else {
-        console.table(res.rows);
-    }
-    con.end();
+const con = new Client({
+    host: 'localhost',
+    user: 'postgres',
+    port: 8000,
+    password: '1234',
+    database: 'bankaccount'
 });
-/*
-fetch('http://localhost:3000/api/accounts')
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Errore nella risposta della rete');
-        }
-        return response.json();
-    })
-    .then(data => {
-        console.table(data);
-    })
-    .catch(error => {
-        console.error('Errore nella fetch:', error);
-    });*/
+
+con.connect()
+    .then(() => console.log("Connected"))
+    .then(() => {
+        con.query('SELECT * FROM account WHERE id=1', (err, res) => {
+            if (err) {
+                console.error('Errore nella query:', err);
+            } else {
+                console.log(JSON.stringify(res.rows, null, 2));
+            }
+            con.end();
+        });
+    })    .catch(err => console.error('Errore nella connessione:', err));
